7cc526a6aa118ded19ba29f8bd4dbed366757b7d
dlschon0@gmail.com <dlschon0@gmail.com@69e79acd-24eb-2ebd-944f-b3b66ffac96d>
2013-06-21 04:17:15 +0000

    Added ScrollingBackground.java. It takes an array of Sprites as a parameter and makes a continuously scrolling background out of them.

--- trunk/src/com/example/skeletonbears/MainActivity.java
+++ trunk/src/com/example/skeletonbears/MainActivity.java
@@ -1,22 +1,101 @@
 package com.example.skeletonbears;
 
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.SpriteBackground;
+import org.andengine.entity.sprite.ButtonSprite;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SimpleBaseGameActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	/**
+	 * The main Android Activity where the game begins
+	 */
+	
+	//=====================
+	//Constants
+	//=====================
+	
+		public static int CAMERA_WIDTH = 1920;
+		public static int CAMERA_HEIGHT = 1080;
+	
+	//=====================
+	//Fields
+	//=====================	
+	
+		/* Graphics */
+			BitmapTextureAtlas atlBG1;
+			ITextureRegion texBG1;
+			
+			BitmapTextureAtlas atlBG2;
+			ITextureRegion texBG2;
+		
+		/* Sprites */
+			ScrollingBackground scrBackground;
+			Sprite sprBG1;
+			Sprite sprBG2;
+			
+		/* Misc */
+			public Camera camera;
+			public Scene scene;
+			
+	public EngineOptions onCreateEngineOptions() 
+	{
+		camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+		engineOptions.getTouchOptions().setNeedsMultiTouch(true);
+		return engineOptions;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onCreateResources() 
+	{
+		//Create Resources
+		atlBG1 = new BitmapTextureAtlas(this.getTextureManager(), 1920, 1080, TextureOptions.BILINEAR);
+		texBG1 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(atlBG1, this, "kitties1.jpg", 0, 0);
+		
+		atlBG2 = new BitmapTextureAtlas(this.getTextureManager(), 1920, 1080, TextureOptions.BILINEAR);
+		texBG2 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(atlBG2, this, "kitties2.png", 0, 0);
+		
+		//Load them up
+		atlBG1.load();
+		atlBG2.load();
+	}
+
+	@Override
+	protected Scene onCreateScene() 
+	{
+		scene = new Scene();
+		scene.setTouchAreaBindingOnActionDownEnabled(true);
+		
+		sprBG1 = new Sprite(0, 0, texBG1, this.getVertexBufferObjectManager());
+		sprBG2 = new Sprite(0, 0, texBG2, this.getVertexBufferObjectManager());
+		
+		/* Create the background */
+		Sprite[] bgSprites = 
+			{
+				sprBG1,
+				sprBG2
+			};
+		scrBackground = new ScrollingBackground(bgSprites, 10, scene);
+		
+		//Attach them to the scene
+		scene.attachChild(scrBackground);
+		
+		return scene;
 	}
 
 }
